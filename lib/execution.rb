531158ffe2a84974b78d5bfc5b880541f10bac11
Jason Reeder <info@jasonreeder.com>
2017-02-02 11:35:44 -0800

    Add code for #my_min

--- lib/execution.rb
+++ lib/execution.rb
@@ -1,3 +1,12 @@
 def my_min(arr)
-  
+  arr.each do |el1|
+    el1_smaller = true
+
+    arr.each do |el2|
+      next if el1 == el2
+      el1_smaller = false unless el1 < el2
+    end
+
+    return el1 if el1_smaller
+  end
 end
